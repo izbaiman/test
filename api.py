7c209f601d61ff1411b36db4dea21a2f6c07696b
izbaiman <118533435+izbaiman@users.noreply.github.com>
2025-01-29 10:53:25 +0000

    my first commit

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+# app.py
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,3 +6,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/api/greet", methods=["GET"])
+def greet():
+    return "message"
+
+if __name__ == "__main__":
+    app.run(debug=True)
